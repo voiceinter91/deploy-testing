bc5311b9b86bb408adf6e27026c6005d621dc046
nooreqabee2 <nooreqabee2@gmail.com>
2022-07-22 17:42:06 +0300

    first commit

---
+++ deploy/views.py
@@ -0,0 +1,10 @@
+from operator import index
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    pass
+
+    return render(request , 'deploy/index.html', context={
+
+    })
